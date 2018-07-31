972bf5e9206875869e25ffb7c35f5c30aca6a6ac
Duy Hoang <baoduy2412@outlook.com>
2018-07-31 22:12:06 +0800

    update config

--- webpack/common.js
+++ webpack/common.js
@@ -1,34 +1,34 @@
 // shared config (dev and prod)
-const { resolve } = require("path");
+const { resolve } = require('path');
 
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const webpack = require("webpack");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
   resolve: {
-    extensions: [".js", ".jsx"]
+    extensions: ['.js', '.jsx']
   },
-  context: resolve(__dirname, "../../src"),
+  context: resolve(__dirname, '../../src'),
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
         use: {
-          loader: "babel-loader"
+          loader: 'babel-loader'
         },
         exclude: /node_modules/
       },
       {
         test: /\.css$/,
         use: [
-          "style-loader",
+          'style-loader',
           {
-            loader: "css-loader",
+            loader: 'css-loader',
             options: {
               importLoaders: 1
             }
           },
-          "postcss-loader"
+          'postcss-loader'
         ]
       },
       {
@@ -37,13 +37,13 @@ module.exports = {
             test: /\.less$/,
             use: [
               {
-                loader: "style-loader" // creates style nodes from JS strings
+                loader: 'style-loader' // creates style nodes from JS strings
               },
               {
-                loader: "css-loader" // translates CSS into CommonJS
+                loader: 'css-loader' // translates CSS into CommonJS
               },
               {
-                loader: "less-loader" // compiles Less to CSS
+                loader: 'less-loader' // compiles Less to CSS
               }
             ]
           }
@@ -51,53 +51,53 @@ module.exports = {
       },
       {
         test: /\.(jpe?g|png|gif)$/i,
-        use: ["file-loader?name=img/[name].[ext]", "img-loader"]
+        use: ['file-loader?name=img/[name].[ext]', 'img-loader']
       },
       {
         test: /\.(svg)$/i,
-        use: ["url-loader?mimetype=image/svg+xml", "img-loader"]
+        use: ['url-loader?mimetype=image/svg+xml', 'img-loader']
       },
       {
         test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
-        loader: "file-loader",
+        loader: 'file-loader',
         options: {
-          name: "fonts/[name].[ext]"
+          name: 'fonts/[name].[ext]'
         }
       },
       {
         test: /\.(woff|woff2)$/,
-        loader: "file-loader?name=fonts/[name].[ext]"
+        loader: 'file-loader?name=fonts/[name].[ext]'
       },
       {
         test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
-        loader: "url-loader?mimetype=application/octet-stream"
+        loader: 'url-loader?mimetype=application/octet-stream'
       },
       //copy Web config file to dist folder
       {
         test: /web.config/,
-        loader: "file-loader?name=[name].[ext]"
+        loader: 'file-loader?name=[name].[ext]'
       }
     ]
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "index.html.ejs",
-      favicon: "favicon.ico"
+      template: 'index.html.ejs',
+      favicon: 'favicon.ico'
     }),
     new webpack.ProvidePlugin({
-      React: "react",
-      ReactDOM: "react-dom"
+      React: 'react',
+      ReactDOM: 'react-dom'
     })
   ],
   externals: {
-    React: "react",
-    ReactDOM: "react-dom",
-    jsdom: "window",
-    "react/addons": true,
-    "react/lib/ExecutionEnvironment": true,
-    "react/lib/ReactContext": "window",
-    "react-dom/test-utils": true,
-    "react-test-renderer/shallow": true
+    React: 'react',
+    ReactDOM: 'react-dom',
+    jsdom: 'window',
+    'react/addons': true,
+    'react/lib/ExecutionEnvironment': true,
+    'react/lib/ReactContext': 'window',
+    'react-dom/test-utils': true,
+    'react-test-renderer/shallow': true
   },
   performance: {
     hints: false
